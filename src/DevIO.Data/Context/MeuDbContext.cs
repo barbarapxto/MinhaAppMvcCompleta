b74497fa40a9e0ca26902db7b0c22f23294aa814
Bárbara Peixoto <barbarapeixoto94@hotmail.com>
2021-01-23 17:07:57 -0300

    Criação de contexto e mapeamento das entidades.

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,40 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //foreach(var property in modelBuilder.Model.GetEntityTypes()
+            //    .SelectMany(e => e.GetProperties()
+            //    .Where(p => p.ClrType == typeof(string))))
+            //    property.Relational().ColumnType = "varchar(100)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //impede uma classe de excluir "seus filhos" quando um item for excluído
+            //ex.: ao excluir um fornecedor, seriam excluídos todos os produtos relacionados a ele.
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+
+            base.OnModelCreating(modelBuilder); 
+        }
+
+    }
+}
